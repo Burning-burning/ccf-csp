ba1c226238876c70d496dc20daaa43ac21ab7d84
Burning-burning <1054689185@qq.com>
2020-11-15 11:19:33 +0800

    201512 第一题

---
+++ src/test201512/First.java
@@ -0,0 +1,17 @@
+package test201512;
+
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int sum = 0;
+        while(n>0){
+            sum+=n%10;
+            n/=10;
+        }
+        System.out.println(sum);
+    }
+}
